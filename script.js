29f1cd98aaef79be2141feb713f0525b52381313
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-06-02 10:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+function fuc1()
+{
+    let url=document.getElementById("url")
+    let name=document.getElementById("name")
+    let year=document.getElementById("year")
+    let submit=document.getElementById("submit")
+
+    console.log(name.value)
+    url.innerText="https://localhost:8080/?"
+    if(name.value=="")
+    {
+    
+    }
+    else
+    {
+        url.innerText=url.innerText+"name="+name.value
+        if(year.value!="")
+        {
+            url.innerText=url.innerText+"&year="+year.value
+        }
+    }
+}
